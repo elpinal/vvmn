f9ec6527e29cb6631211483c7406cab2d538e58e
elpinal <elpinal@users.noreply.github.com>
2016-06-30 19:00:32 +0900

    Add use command

--- use.go
+++ use.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"path"
+
+	"github.com/pkg/errors"
+)
+
 var cmdUse = &Command{
 	Run:       runUse,
 	UsageLine: "use ",
@@ -16,6 +24,38 @@ func init() {
 
 // runUse executes use command and return exit code.
 func runUse(args []string) int {
+	if len(args) == 0 {
+		fmt.Fprintln(os.Stderr, "vvmn use: no vim version specified")
+		return 1
+	}
+	currentDir := path.Join(VvmnDir, "vims", "current")
+	version := args[0]
+	if version == "system" {
+		if _, err := os.Stat(currentDir); err != nil {
+			return 0
+		}
+		if err := os.RemoveAll(currentDir); err != nil {
+			fmt.Fprintln(os.Stderr, errors.Wrap(err, "failed use system version of vim"))
+			return 1
+		}
+		return 0
+	}
+	vimsDir := path.Join(VvmnDir, "vims", version)
+	if _, err := os.Stat(vimsDir); err != nil {
+		fmt.Fprintln(os.Stderr, errors.Wrap(err, "no installed version of vim specified"))
+		return 1
+	}
+	if _, err := os.Stat(currentDir); err == nil {
+		if err := os.RemoveAll(currentDir); err != nil {
+			fmt.Fprintln(os.Stderr, errors.Wrap(err, "failed unuse former version of vim"))
+			return 1
+		}
+	}
+	err := os.Symlink(vimsDir, currentDir)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, errors.Wrap(err, "failed create symbolic link"))
+		return 1
+	}
 
 	return 0
 }
