76f6abb7f5cb1f22be791ad26a3e509a84434709
elpinal <elpinal@users.noreply.github.com>
2016-09-13 19:34:00 +0900

    Import color

--- cmd/vvmn/main.go
+++ cmd/vvmn/main.go
@@ -1,37 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"io"
 	"os"
 
+	"github.com/elpinal/color"
 	"github.com/susp/vvmn/cli"
 )
 
-type colorWriter struct {
-	w     io.Writer
-	color int
-}
-
-func (c *colorWriter) Write(b []byte) (int, error) {
-	var n int
-	if _, err := io.WriteString(c.w, fmt.Sprintf("\033[%vm", c.color)); err != nil {
-		return n, err
-	}
-	n0, err := c.w.Write(b)
-	n += n0
-	if err != nil {
-		return n, err
-	}
-	if _, err := io.WriteString(c.w, "\033[0m"); err != nil {
-		return n, err
-	}
-	return len(b), nil
-}
-
 func main() {
-	yw := &colorWriter{w: os.Stdout, color: 33}
-	rw := &colorWriter{w: os.Stdout, color: 31}
+	yw := color.New(os.Stdout, color.Yellow)
+	rw := color.New(os.Stdout, color.Red)
 	c := cli.CLI{OutStream: yw, ErrStream: rw}
 	r := c.Run(os.Args[1:])
 	os.Exit(r)
